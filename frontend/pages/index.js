37c2640a19ae73327be48b768eb6faf3c1870011
primboi <81822069+primitt@users.noreply.github.com>
2022-12-04 22:27:08 -0800

    (wip) started work on home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,9 +2,22 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/navbar";
 import { useUser } from "../components/user";
+import { useEffect, useState } from "react";
+import api from "../api";
+import { CircularProgress } from "@mui/material";
 
 export default function Home() {
     const { user } = useUser();
+    const [loading, setLoading] = useState(true);
+    const [serversInfo, setServersInfo] = useState(null);
+    useEffect(() => {
+        api.getServers(0) // TODO: Get ?page param and pass it to api.getServers()
+            .then((data) => {
+                setServersInfo(data.data);
+                setLoading(false);
+            })
+            .catch(() => setLoading(false));
+    });
 
     return (
         <>
@@ -28,9 +41,20 @@ export default function Home() {
                 <meta property="og:type" content="website" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className={styles.root}>
+            <div className={styles.root} style={{marginBottom:"20px"}}>
                 <Navbar />
             </div>
+            {loading ? (
+                <CircularProgress />
+            ) : (
+                <>
+                    {serversInfo.map((server)=> (
+                        <div class={styles.box}>
+                            <h2 className={styles.center}>{server.name}</h2>
+                        </div>
+                    ))}
+                </>
+            )}
         </>
     );
 }
