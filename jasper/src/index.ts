d90bf2bc29bb7ff09f9da2fa8d0d9cf428c59b47
ColdDev <colddev@pm.me>
2022-12-09 01:05:28 +0000

    feat(jasper): fix player count analytic

--- jasper/src/index.ts
+++ jasper/src/index.ts
@@ -1,62 +1,95 @@
 import { PrismaClient, AnalyticType, Server } from "@prisma/client";
 import { WebSocket } from "ws";
-import createLogger from "logging"
+import createLogger from "logging";
 const prisma = new PrismaClient();
 import { CronJob } from "cron";
 
-const logger = createLogger('Jasper')
+const logger = createLogger("Jasper");
 
-enum types{
-    PLAYER_COUNT,
-}
+// not sure why this is needed, uncomment if this is required
+// enum types {
+//     PLAYER_COUNT,
+// }
+
+type ServerResponse = {
+    data: {
+        motd: string[];
+        cache: boolean;
+        max: number;
+        players: string[];
+        icon: boolean;
+        online: number;
+    };
+    vers: string;
+    name: string;
+    time: BigInt | number;
+    type: string;
+    secure: boolean;
+    brand: string;
+    uuid: string;
+    cracked: boolean;
+};
+
 const verifyConnection = () =>
     new Promise<void>(async (resolve, reject) => {
         await prisma.user.findMany().catch(reject);
         resolve();
     });
-function runAnalyticPlayerCount() {
+
+const safelyParseJSON = (json: string): unknown => {
+    let parsed: string;
+
+    try {
+        parsed = JSON.parse(json);
+    } catch (e) {
+        return null;
+    }
+
+    return parsed;
+};
+
+const runAnalyticPlayerCount = () => {
+    logger.info("Running player count analytic...");
     verifyConnection().then(async () => {
-        logger.info("Verified connection to database");
-        const server = await prisma.server.findMany({
+        const servers = await prisma.server.findMany({
             where: {
                 disabled: false,
                 approved: true,
             },
         });
-        server.forEach(async (serverInfo: Server) => {
+        servers.forEach(async (serverInfo: Server) => {
             const ws = new WebSocket(`${serverInfo.address}`);
-            logger.info(`Connecting to ${serverInfo.address}`)
-            ws.onopen = () => {
-                ws.send("Accept: MOTD");
-            };
+            logger.info(`Connecting to ${serverInfo.address}...`);
+            ws.onopen = () => ws.send("Accept: MOTD");
             ws.on("message", async (msg) => {
-            logger.info(`Connected to ${serverInfo.address}`)
-            const data = msg.data;
-            const find = await prisma.analytic.findFirst({
-                where:{
-                    serverId: serverInfo.uuid,
-                }
-            })
-            console.log(msg)
-            if (!find){
+                ws.close();
+                logger.info(`Connected to ${serverInfo.address}`);
+                const data = (safelyParseJSON(msg.toString()) as ServerResponse)
+                    .data;
+                if (!data)
+                    return logger.warn(
+                        `Server ${serverInfo.address} returned invalid JSON data, skipping collection.`
+                    );
                 await prisma.analytic.create({
-                    data:{
+                    data: {
                         serverId: serverInfo.uuid,
                         type: AnalyticType.PLAYER_COUNT,
-                        data: msg.online,
-                    }
+                        data: data.online.toString(),
+                    },
                 });
-                console.log("Created!")
-            }
-            ws.close();
-        });
+                logger.info(
+                    `Successfully collected player count analytics for ${serverInfo.address}`
+                );
+            });
             ws.onerror = () => {
-                logger.warn(`Unable to connect to ${serverInfo.address}`)
+                logger.warn(`Unable to connect to ${serverInfo.address}`);
                 ws.terminate();
-            }
+            };
         });
     });
-}
+};
 
-const job = new CronJob("59 * * * * *", runAnalyticPlayerCount);
-job.start();
+runAnalyticPlayerCount();
+
+// const job = new CronJob("59 * * * * *", runAnalyticPlayerCount);
+// job.start();
