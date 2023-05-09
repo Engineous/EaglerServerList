20fbc791679be24d3753bf0deb07d06b52b9e02d
ColdDev <colddev@pm.me>
2023-05-09 21:05:52 +0100

    feat(voting): move vote cooldown check from backend to jasper

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -32,26 +32,4 @@ verifyConnection().then(() => {
     listen(parseInt(process.env.PORT!)).then(() =>
         logger.info(`Successfully running on port ${process.env.PORT!}`)
     );
-    setInterval(async () => {
-        logger.info("Running vote cooldown check...");
-        const cooldowns = await prisma.voteCooldown.findMany().catch(() => {});
-        if (!cooldowns) return;
-        await Promise.all(
-            cooldowns.map(async (cooldown) => {
-                if (cooldown.expiresAt < new Date())
-                    await prisma.voteCooldown
-                        .delete({
-                            where: {
-                                uuid: cooldown.uuid,
-                            },
-                        })
-                        .then(() =>
-                            logger.info(
-                                `Successfully removed vote cooldown for user ${cooldown.userId} and server ${cooldown.serverId}`
-                            )
-                        )
-                        .catch(() => {});
-            })
-        );
-    }, 60000);
 });
